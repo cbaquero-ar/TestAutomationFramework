6f15bcdaeda7a2f71ec452ac1a1c620b7e584906
cbaquero-ar <baquero.carolina@gmail.com>
2018-03-22 18:03:15 -0300

    Changing tests e2e to javascript tests only. No need to use typescript for the poc

--- test-e2e/index.spec.js
+++ test-e2e/index.spec.js
@@ -1,9 +1,8 @@
-import {browser, element, by} from 'protractor';
-
-describe("Demo Blog", function(){
+describe("Demo Blog App", function(){
     
     //Title
     it('Panel Title should contain expected label', () => {
+        browser.get('http://localhost:3000');
         element(by.css('.navbar-brand')).getText().then(function(blogName){
             expect(blogName).toEqual("demoBlog");
         })
