e19bef7088745e5e50c0aab1869e6eef430284c0
cbaquero-ar <baquero.carolina@gmail.com>
2018-04-02 19:52:47 -0300

    adding sauce settings on sauce-conf.js

--- test-e2e/index.spec.js
+++ test-e2e/index.spec.js
@@ -2,15 +2,16 @@ describe("Demo Blog App", function(){
     
     //Title
     it('Panel Title should contain expected label', () => {
-        // browser.get('http://localhost:5000');
-        // element(by.css('.navbar-brand')).getText().then(function(blogName){
-        //     expect(blogName).toEqual("demoBlog");
-        // });
+        browser.get('http://localhost:5000');
+        element(by.css('.navbar-brand')).getText().then(function(blogName){
+            expect(blogName).toEqual("demoBlog");
+        });
         
-        browser.get('https://angular.io');
-        element(by.css('.button.hero-cta')).getText().then(function(blogName){
-            expect(blogName).toEqual("GET STARTED");
-        });      
+        // Working code on integration circle_ci+sauce_labs
+        // browser.get('https://angular.io');
+        // element(by.css('.button.hero-cta')).getText().then(function(blogName){
+        //     expect(blogName).toEqual("GET STARTED");
+        // });      
     });
 
 });
